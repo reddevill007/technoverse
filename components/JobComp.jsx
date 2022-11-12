ef0f6e109ca364b68728c67faa660e888ad57c64
reddevill007 <shivampandey2900@gmail.com>
2022-11-12 17:21:53 +0530

    job component

--- components/JobComp.jsx
+++ components/JobComp.jsx
@@ -7,7 +7,7 @@ const JobComp = () => {
             <h1>Jobs For You</h1>
             <div className="flex flex-col w-full gap-8 justify-center items-center">
                 {jobsData.map((job) => (
-                    <div className="border rounded-xl w-[80%] p-5" key={job.id}>
+                    <div className="border rounded-xl bg-gradient-to-r from-black via-gray-900 to-black w-[80%] p-5" key={job.id}>
                         <h2 className='text-secondary-100 text-xl font-bold mx-2 my-3'>{job.position}</h2>
                         <h3 className='text-gray-600 text-xl font-bold mb-3'>{job.company}</h3>
                         <div className="flex w-full justify-between text-secondary-900 mx-2 font-bold">
